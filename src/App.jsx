22bf1450a56683c955e7c163e95502c40277d37e
Nandana Santhosh <119744179+Nndna9@users.noreply.github.com>
2023-06-07 15:53:17 +0000

    set homepage for github pages deploy

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Nandana Santhosh",
+  title: "Aspiring AI Engineer",
+  email: "nndna9@gmail.com",
+  gitHub: "Nndna",
+  instagram: " ",
+  linkedIn: "nandana-santhosh",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: " ",
+  youTube: " ",
 };
 
 const primaryColor = "#4E567E";
